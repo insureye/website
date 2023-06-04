b7ca0c73cdaf47f7eee54c009852cecfe15fe9a4
Francisco Cardeñoso <fcocar31@gmail.com>
2023-06-04 21:00:14 +0200

    Client side routing, Added Link component

--- src/components/utils/BtnContactUs.jsx
+++ src/components/utils/BtnContactUs.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from '../../style'
+import { Link } from 'react-router-dom';
 
 const BtnContactUs = ({name, link}) => {
     return(
     <div className={`${styles.btnContactUs} ${styles.btnContactUsHover} flex flex-col items-center justify-center rounded-full p-[1px] min-w-[100px] max-w-[146px]`}>
-        <a href={link} className={`py-2 px-4 w-full h-full rounded-full text-[17px] text-center`}>
+        <Link to={link} onClick={() => link.CharAt(0) == "#" ? location.reload() : null} className={`py-2 px-4 w-full h-full rounded-full text-[17px] text-center`}>
             {name}
-        </a>
+        </Link>
     </div>);
 }
 
