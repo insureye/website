3eb8628a5e4aee3f3f3f0c8bfd2cf2efcc1456f6
Francisco Cardeñoso <fcocar31@gmail.com>
2023-06-04 21:16:04 +0200

    CR

--- src/components/utils/BtnContactUs.jsx
+++ src/components/utils/BtnContactUs.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import styles from '../../style'
 import { Link } from 'react-router-dom';
 
-const BtnContactUs = ({name, link}) => {
+const BtnContactUs = ({name, link, reload=true}) => {
     return(
     <div className={`${styles.btnContactUs} ${styles.btnContactUsHover} flex flex-col items-center justify-center rounded-full p-[1px] min-w-[100px] max-w-[146px]`}>
-        <Link to={link} onClick={() => link.CharAt(0) == "#" ? location.reload() : null} className={`py-2 px-4 w-full h-full rounded-full text-[17px] text-center`}>
+        <Link to={link} onClick={() => reload ? window.location = "#ContactUs" : null} className={`py-2 px-4 w-full h-full rounded-full text-[17px] text-center`}>
             {name}
         </Link>
     </div>);
